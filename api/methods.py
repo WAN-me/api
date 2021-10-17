ca0b42ddbea840e5201c50bb67d32bfa4c573fbb
wex335 <wex335@yandex.ru>
2021-10-17 18:02:31 +0300

    fix db

--- api/methods.py
+++ api/methods.py
@@ -3,7 +3,7 @@ import hashlib,time
 
 NEW_BD = '''CREATE TABLE users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL)
+            name TEXT NOT NULL,
             token TEXT NOT NULL,
             email,
             password,
