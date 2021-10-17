78825ea36456c9cd680e463f767137fbf3704b34
wex335 <wex335@yandex.ru>
2021-10-17 11:33:58 +0300

    .

--- api/methods.py
+++ api/methods.py
@@ -1,7 +1,41 @@
 from sqlite3 import Error, connect
 import hashlib,time
+
+NEW_BD = '''CREATE TABLE users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            token TEXT NOT NULL,
+            online_state TEXT NOT NULL,
+            name TEXT NOT NULL);'''
+
+
 ############################
 ###user
+############################
+def userget(args):
+    ss = notempty(args,['accesstoken'])
+    token = args['accesstoken']
+    if ss!=True:#токена нет
+        return ss
+    else: 
+        user = (db(f'''select id,name,token,online_state from users where token = "{token}" '''))
+        if not user or len(user)!=1:
+            return error(2,"'accesstoken' is invalid")
+        else:
+            return {'id':user[0][0],'name':user[0][1],'token':user[0][2],'online_state':user[0][3]}
+
+def reg(args):
+    ss = notempty(args,['name'])
+    if ss == True:
+        name = args['name']
+        token = hashlib.sha256(f'{name}_{time.time()}'.encode()).hexdigest()
+        print(db(f'''insert into users (name,token)
+        values ('{name}','{token}')'''))
+        return userget({'accesstoken':token})
+    else: return ss
+
+
+############################
+###database
 ############################
 def db(query):
     print(query)
@@ -14,44 +48,17 @@ def db(query):
     c.close
     return res
 
-def userget(args):
-    token = args.get('accesstoken')
-    user = (db(f'''select id,name,token from users where token = "{token}" '''))
-    if not token or token=="":#токена нет
-        return {'error':{'code':1,'text':'\'accesstoken\' is empty'}}
 
-    elif not user or len(user)!=1:
-        return {'error':{"code":2,"text":"'accesstoken' is invalid"}}
-
-    else:
-        return {'id':user[0][0],'name':user[0][1],'token':user[0][2],}
-
-def reg(args):
-    name = args.get('name')
-    if not name or name=='':
-        return {'error':{"code":3,"text":"'name' is empty"}}
-    token = hashlib.sha256(f'{name}_{time.time()}'.encode()).hexdigest()
-    print(db(f'''insert into users (name,token)
-    values ('{name}','{token}')'''))
-    return userget({'accesstoken':token})
-
-
-############################
-###database
-############################
 def dropdatabase(args):
     user = userget(args)
     print(user)
     if not 'error' in user:
         if user['id'] == 1:
             db('''DROP table users ''')
-            db('''CREATE TABLE users(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            token TEXT NOT NULL,
-            name TEXT NOT NULL);''')
+            db(NEW_BD)
             return {'state':'done'}
         else:
-            return {'error':{"code":4,"text":"access denided for this method"}}
+            return error(4,"access denided for this method")
     else: return user
 
 def dropdatabase(yes:str):
@@ -61,3 +68,22 @@ def dropdatabase(yes:str):
     token TEXT NOT NULL,
     name TEXT NOT NULL);''')
     return {'state':'done'}
+
+
+############################
+###utils
+############################
+
+def error(code,text):
+    return {'error':{"code":code,"text":text}}
+
+
+def notempty(args,need):
+    empty=[]
+    for key in need:
+        name = args.get(key)
+        if not name or name=='':
+            empty.append(key)
+    if len(empty)==0:
+        return True
+    else: return error(3,f"this keys is empty: {empty}")
