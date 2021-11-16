109ef72fb44bb0142ebfdc31292a4ee095569f8a
wex335 <wex335games@yandex.ru>
2021-11-16 15:42:34 +0300

    try fix chats

--- methods/chats.py
+++ methods/chats.py
@@ -11,12 +11,14 @@ def get(args):
         count = args.get('count',10)
         ofset = args.get('ofset',0)
         thisuser = users._gett(token)
+        print("consts init")
         if 'error' in thisuser:
             return thisuser 
         chats = []
         raw_chats = db.exec('''select DISTINCT id from chats where user_id=:userId 
                 order by id desc limit :ofset,:count''',
                 {'userId':thisuser[0],'count':count,'ofset':ofset})
+        print("raw_chats init")
         if len(raw_chats)<1:
             return {'count':len(raw_chats),'items':raw_chats}
         for i in raw_chats:
