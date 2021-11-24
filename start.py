9e14be20e08ebc3d8c1dc0fbc4123c06a079eca7
wex335 <wex335games@yandex.ru>
2021-11-24 19:41:22 +0300

    add start scripts

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import os
 from gunicorn.app.wsgiapp import run
 
 if __name__ == '__main__':
-    os.chdir("/root/tests/wm-api/")
-    sys.argv = [sys.argv[0], '-w', '8', '-b', '0.0.0.0:5000', 'api:api','--access-logfile=-']
+    os.chdir("/root/wm-api/")
+    os.system('git pull origin master')
+    sys.argv = [sys.argv[0], '-w', '8', '-b', '0.0.0.0:3000', 'api:api','--access-logfile=-']
     sys.exit(run())
