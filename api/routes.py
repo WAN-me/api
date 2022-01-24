169469331c982cf6d5a4aacb5d0b39ecc7304de9
wex335 <wex335@yandex.ru>
2022-01-24 00:06:34 -0500

    fix params

--- api/routes.py
+++ api/routes.py
@@ -22,15 +22,6 @@ def pageNotFound(error):
 @api.errorhandler(500)
 def ISE(error):
     return methods.utils.error(500,'internal server error'),500
-@api.route('/favicon.ico')
-def favicon():
-    return redirect("https://wan-group.ru/favicon.svg", code=302)
-@api.route('/index', methods=['GET',"POST"])
-def index():
-    return "You in index"
-@api.route('/webpwnchat', methods=['GET',"POST"])
-def webpwn():
-    return "ООО, да вы программист на html"
 
 @api.route('/kino/<method>/<params>', methods=['GET',"POST"])
 def uu1(method,params):
@@ -47,40 +38,12 @@ def uu3(method):
     print(method)
     return methods.kino.universal(request.args.to_dict(),method)
 
-@api.route('/cloud', methods=['GET',"POST"])
-def upload():
-    if request.method == 'POST':
-        if 'file1' not in request.files:
-            return 'there is no file1 in form!'
-        file1 = request.files['file1']
-        print(dir(file1))
-        path = os.path.join(api.config['UPLOAD_FOLDER'], uti.secure_filename(file1.filename))
-        file1.save(path)
-        return redirect("https://cloud.wan-group.ru/upload/"+path.split('/var/www/cloud/upload/',1)[1], code=301)
-    return '''
-    <h1>Upload new File</h1>
-    <form method="post" enctype="multipart/form-data">
-      <input type="file" name="file1">
-      <input type="submit">
-    </form>
-    '''
-@api.route('/auth', methods=['GET',"POST"])
-def auth():
-    args = request.args.to_dict()
-    if "code" in args:
-        code = args['code']
-        r = json.loads(requests.get('https://oauth.vk.com/access_token?client_id=8012324&client_secret=COH80Oy3E72Nhj7ChB7L&redirect_uri=http://test.api.wan-group.ru/auth&code='+code).content)
-        user = json.loads(requests.get(f"https://api.vk.com/method/users.get?access_token={r['access_token']}&v=5.101").content)['response'][0]
-        return f"Привет, {user['first_name']} {user['last_name']}\n{r['email']} - Это твоя почта?"
-    else:
-        return redirect("https://oauth.vk.com/authorize?client_id=8012324&display=page&redirect_uri=http://test.api.wan-group.ru/auth&scope=email&response_type=code&v=5.131", code=302)
 @api.route('/method/<method>/<submethod>', methods=['GET',"POST"])
 @api.route('/method/<method>/<submethod>/', methods=['GET',"POST"])
 @api.route('/method/<method>.<submethod>', methods=['GET',"POST"])
 @api.route('/method/<method>.<submethod>/', methods=['GET',"POST"])
 def methodhandler(method,submethod):
-    args = request.form
-    args['password'] = request.headers.get('password',args.get("password"))
+    args = request.args.to_dict().update(request.form.to_dict())
     method = method.lower()
     submethod = submethod.lower()
     res = "unknown"
