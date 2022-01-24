0a38accb065663ac729a4594b8899d227f5adf5d
wex335 <wex335@yandex.ru>
2022-01-24 09:31:33 +0400

    fix start

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import os
 from gunicorn.app.wsgiapp import run
 
 if __name__ == '__main__':
-    os.chdir("/root/wm-api/")
+    os.chdir("/root/api/")
     os.system('git pull origin master')
     sys.argv = [sys.argv[0], '-w', '8', '-b', '0.0.0.0:3000', 'api:api','--access-logfile=-']
     sys.exit(run())
