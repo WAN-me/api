def324965ce8de13d1a3745d63fc0624fa6063f1
wex335 <wex335@yandex.ru>
2022-04-13 07:16:50 +0300

    edit stderr

--- test.sh
+++ test.sh
@@ -30,32 +30,36 @@ PORT=33030
 # Pre tasks
 echo "branch $BRANCH"
 # download repo
-mkdir ~/.apitemp
-cd ~/.apitemp
+mkdir ~/.apitemp 2> ~/test.out
+cd ~/.apitemp 2> ~/test.out
 
-git clone https://github.com/WAN-me/api
+git clone https://github.com/WAN-me/api 2> ~/test.out
 
-cd api
-git checkout $BRANCH
+cd api 2> ~/test.out
+git checkout $BRANCH 2> ~/test.out
 fuser $PORT/tcp -k
 # create cfg
 
-cat example.cfg.py | sed "s,^api_port = .*,api_port = $PORT,g" > cfg.py
+cat example.cfg.py | sed "s,^api_port = .*,api_port = $PORT,g" > cfg.py 2> ~/test.out
 
 TGTOKEN="TG TOKEN"
 TGCHAT="TG CHAT"
 
 
-STD=$(python3 test.py $PORT) \
+STDt=`python3 test.py $PORT 2> ~/test.out` \
 && ( 
     echo 'test successful!' 
     curl "https://api.telegram.org/bot$TGTOKEN/sendMessage?chat_id=$TGCHAT&text=test+succesful+on+branch+$BRANCH"
-    cd ~/$BRANCH/api
-    ( git pull && curl "https://api.telegram.org/bot$TGTOKEN/sendMessage?chat_id=$TGCHAT&text=test+ok+pull"
+    cd ~/$BRANCH/api  2> ~/test.out
+    ( STDp=`git pull 2> ~/test.out` && curl "https://api.telegram.org/bot$TGTOKEN/sendMessage?chat_id=$TGCHAT&text=test+ok+pull"
     ) || ( curl "https://api.telegram.org/bot$TGTOKEN/sendMessage?chat_id=$TGCHAT&text=failed+pull" ) 
     ) || ( 
     echo 'Test failed' 
-    TEXT="testing fail on branch $BRANCH\n<code>$STD</code>"
+    TEXT="testing fail on branch $BRANCH
+    <code>$STDt</code>
+    <code>$STDp</code>
+    <code>$(cat ~/test.out)</code>
+    "
     MSG=$( phpurlencode "$TEXT" )
     curl "https://api.telegram.org/bot$TGTOKEN/sendMessage?chat_id=$TGCHAT&parse_mode=HTML&text=$MSG"
     
